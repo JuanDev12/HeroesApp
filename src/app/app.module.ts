af92f14b32fe614ca28097f04458025149702dee
Juan Duarte <juanduarte1223@outlook.es>
2023-03-22 19:47:56 -0500

    Download json-server and implementing some new functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 // Own modules
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
